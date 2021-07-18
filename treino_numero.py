c570319f6b0f5d2c7fb5bf1283e2e422de0f62c1
Jefferson Luiz Seno <65913458+JSeno@users.noreply.github.com>
2021-07-18 16:38:16 -0300

    Add files via upload

---
+++ treino_numero.py
@@ -0,0 +1,7 @@
+num = int(input("Digite um número: "))
+if num > 10:
+	print('Seu número é maior que 10')
+elif num < 10:
+	print('Seu número é menor que 10')
+elif num == 10:
+	print("Seu número é 10")
